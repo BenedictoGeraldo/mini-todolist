48e4fd35ba0bb53bf83e08afcbb0f95c1ded435b
BenedictoGeraldo <geraldodawa@gmail.com>
2026-03-04 23:08:03 +0700

    update layouting todo page

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -4,7 +4,7 @@ import TodoList from "./components/todoList";
 export default function TodoPage() {
   return (
     <div className="min-h-screen flex flex-col p-6 md:p-12 lg:p-16">
-      <div className="mb-8">
+      <div className="mb-8 text-center">
         <h1 className="text-4xl font-light">My Tasks</h1>
         <p className="text-gray-500 text-sm mt-1">
           What needs to be done today?
